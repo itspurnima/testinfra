c431583a13bb8441ae999a3fae3f00b38bafad24
itspurnima <61597739+itspurnima@users.noreply.github.com>
2021-08-23 12:33:28 +0100

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
-<?php
-echo "welcome to php";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>My First PHP Web Page</title>
+    </head>
+     <body> 
+          <?php print('Hello World!'); ?>
+     </body>
+</html>
